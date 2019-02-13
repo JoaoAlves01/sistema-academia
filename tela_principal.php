30bbea3787a762a9fe7d83737cf2315a05670e39
Joao Alves <joaoalvesjc16@gmail.com>
2019-02-13 20:52:01 -0200

    criacao da interface de alimentacao do quiosque

--- tela_principal.php
+++ tela_principal.php
@@ -12,64 +12,93 @@
         <script src="lib/js/controle.js"></script>
     </head>
     <body>
-        <header>
-            <div class="menu_sistema">
-                <div class="img_usuario_sistema">
-                    <img src="imagens/0001.png" alt="usuario_sistema" />
-                </div>
-                <div class="linha">
-                    <span class="nome_usuario">Boa Tarde,<small>Joao Pedro Alves de Sousa</small></span>
-                </div>
-
-                <nav id="menu_sistema">
-                    <ul>
-                        <li><i class="fa fa-desktop" aria-hidden="true"><a href="#">Quiosque</a></i></li>
-                        <li><i class="fa fa-clone" aria-hidden="true"><a href="#">Planos</a></i></li>
-                        <li><i class="fa fa-quote-right" aria-hidden="true"><a href="#">Depoimentos</a></i></li>
-                        <li><i class="fa fa-home" aria-hidden="true"><a href="#">Blog</a></i></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
-
-        <section>
-            <div class="barra_info">
-                <div class="notificacao_usuario">
-                    <i class="fa fa-envelope-o" aria-hidden="true"><span>6</span></i>
-                </div>
-            </div>
-
-            <div class="envelope">
-                <h1 class="titulo_formulario">Administrar Quiosque</h1>
-                
-                <div class="envelope_formulario">
-                    <h2 class="titulo_sub_formulario">Fazer upload de imagens</h2>
+        <div class="envelope_body">
+            <header>
+                <div class="menu_sistema">
+                    <div class="img_usuario_sistema">
+                        <img src="imagens/0001.png" alt="usuario_sistema" />
+                    </div>
                     <div class="linha">
-                        <div class="esquerda_update">
-                            <input type="file" class="update_arquivo" />
+                        <span class="nome_usuario">Boa Tarde,<small>Joao Pedro Alves de Sousa</small></span>
+                    </div>
+
+                    <nav id="menu_sistema">
+                        <ul>
+                            <li><i class="fa fa-desktop" aria-hidden="true"><a href="#">Quiosque</a></i></li>
+                            <li><i class="fa fa-clone" aria-hidden="true"><a href="#">Planos</a></i></li>
+                            <li><i class="fa fa-quote-right" aria-hidden="true"><a href="#">Depoimentos</a></i></li>
+                            <li><i class="fa fa-home" aria-hidden="true"><a href="#">Blog</a></i></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+
+            <section>
+                <div class="barra_info">
+                    <div class="notificacao_usuario">
+                        <i class="fa fa-envelope-o" aria-hidden="true"><span>6</span></i>
+                    </div>
+                </div>
+
+                <div class="envelope">
+                    <h1 class="titulo_formulario">Administrar Quiosque</h1>
+                    
+                    <div class="envelope_formulario">
+                        <h2 class="titulo_sub_formulario">Fazer upload de imagens</h2>
+                        <div class="linha">
+                            <div class="esquerda_update">
+                                <input type="file" class="update_arquivo" />
+                            </div>
+
+                            <div class="direita_update">
+                                <div class="conter_campos_formulario">
+                                    <label class="label_sistema" for="nome_img_update">Nome</label>
+                                    <input type="text" class="campo_sistema" id="nome_img_update">
+
+                                    <div class="linha">
+                                        <button type="submit" class="botao botao_azul">Enviar</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
+                    </div>
+                    <br>
+                    <div class="envelope_formulario">
+                        <h2 class="titulo_sub_formulario">Anúncios em destaque</h2>
+                        <div class="linha">
+                            <div class="box_contato">
+                                <h1>Del Rio Fitness</h1>
+                                <div class="box_img_anuncio">
+                                    <img class="centralizar_img" src="imagens/anuncio_fitness.jpg" />
+                                </div>
+                                <div class="linha base_box_anuncio">
+                                    <button type="submit" class="botao excluir_botao botao_vermelho">Excluir</button>
+                                </div>
+                            </div>
 
-                        <div class="direita_update">
-                            <div class="conter_campos_formulario">
-                                <label class="label_sistema" for="nome_img_update">Nome</label>
-                                <input type="text" class="campo_sistema" id="nome_img_update">
+                            <div class="box_contato">
+                                <h1>Dentista Zona Sul</h1>
+                                <div class="box_img_anuncio">
+                                    <img class="centralizar_img" src="imagens/anuncio_dentista.jpg" />
+                                </div>
+                                <div class="linha base_box_anuncio">
+                                    <button type="submit" class="botao excluir_botao botao_vermelho">Excluir</button>
+                                </div>
+                            </div>
 
-                                <label class="label_sistema" for="descricao_img_update">Descrição</label>
-                                <textarea rows="4" type="text" class="campo_sistema" id="descricao_img_update"></textarea>
-
-                                <div class="linha">
-                                    <button type="submit" class="botao">Enviar</button>
+                            <div class="box_contato">
+                                <h1>Internet da Nasa</h1>
+                                <div class="box_img_anuncio">
+                                    <img class="centralizar_img" src="imagens/anuncio_internet.png" />
+                                </div>
+                                <div class="linha base_box_anuncio">
+                                    <button type="submit" class="botao excluir_botao botao_vermelho">Excluir</button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <br>
-                <div class="envelope_formulario">
-                    <h2 class="titulo_sub_formulario">Anúncios em destaque</h2>
-                    <input type="file" class="update_arquivo" />
-                </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </body>
 </html>
